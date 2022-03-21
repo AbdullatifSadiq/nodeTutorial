b8505dd8e0b9c19a86a9517374b1edaae9a4e936
Abdul-latif Sadiq <abdul.latif708@yahoo.com>
2022-03-21 16:50:57 -0400

    rename file

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -10,13 +10,25 @@ const path = require('path')
 // })
 
 // Creating and writing to a file
-fs.writeFile(path.join(__dirname,'/test','hello.txt'),'Hello World', err => {
-    if(err) throw err
-    console.log('File written to....')
+// fs.writeFile(path.join(__dirname,'/test','hello.txt'),'Hello World', err => {
+//     if(err) throw err
+//     console.log('File written to....')
 
-    // Appending to a
-    fs.appendFile(path.join(__dirname,'/test','hello.txt'),' I love Node', err => {
-        if(err) throw err
-        console.log('File written to....')
-    })
+//     // Appending to a
+//     fs.appendFile(path.join(__dirname,'/test','hello.txt'),' I love Node', err => {
+//         if(err) throw err
+//         console.log('File written to....')
+//     })
+// })
+
+// Read from a file
+// fs.readFile(path.join(__dirname,'/test', 'hello.txt'),'utf8', (err,data) => {
+//     if(err) throw err
+//     console.log(data)
+// })
+
+// Rename File 
+fs.rename(path.join(__dirname,'/test', 'hello.txt'),path.join(__dirname,'/test', 'helloword.txt'), (err,data) => {
+    if(err) throw err
+    console.log('File renamed...')
 })
