2fc810a85f9f936a9651271276cf86447ae25a65
Abdul-latif Sadiq <abdul.latif708@yahoo.com>
2022-03-21 16:38:26 -0400

    appending to a file

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -13,4 +13,10 @@ const path = require('path')
 fs.writeFile(path.join(__dirname,'/test','hello.txt'),'Hello World', err => {
     if(err) throw err
     console.log('File written to....')
+
+    // Appending to a
+    fs.appendFile(path.join(__dirname,'/test','hello.txt'),' I love Node', err => {
+        if(err) throw err
+        console.log('File written to....')
+    })
 })
