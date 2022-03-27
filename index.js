7e75b66a546e947bd6e8b139dee74511aa42b9e9
Abdul-latif Sadiq <abdul.latif708@yahoo.com>
2022-03-27 17:37:54 -0400

    created a mini server

--- index.js
+++ index.js
@@ -4,24 +4,69 @@ const path = require('path')
 
 
 const server = http.createServer((req,res) =>{
-     if(req.url === '/') {
-          fs.readFile(path.join(__dirname,'public', 'index.html'),(err,content)=>{
-               if(err) throw err
-               res.writeHead(200, {'Content-Type': 'text/html'})
-               res.end(content)
+     // if(req.url === '/') {
+     //      fs.readFile(path.join(__dirname,'public', 'index.html'),(err,content)=>{
+     //           if(err) throw err
+     //           res.writeHead(200, {'Content-Type': 'text/html'})
+     //           res.end(content)
+     //      })
+     // }
+     // if(req.url === '/api/users') {
+     //     const users = [
+     //          {name:'ASDFR', age: 23},
+     //          {name:'qqwee', age: 43}
+     //     ]
+     //     res.writeHead(200, {'Content-Type': 'application/json'})
+     //     res.end(JSON.stringify(users))
+     // }
 
-          })
+     //  Creating a dynamic file path
+     let filePath = path.join(__dirname,'public',req.url === '/' ? 'index.html': req.url)
 
+     // Getting the file extension
+     let extname = path.extname(filePath)
+
+     // Initial content type
+     let contentType = 'text/html'
+
+     // Check ext and set content type
+     switch(extname){
+          case '.js':
+               contentType = 'text/javascript';
+               break;
+          case '.css':
+               contentType = 'text/css';
+               break;
+          case '.json':
+               contentType = 'application/json';
+               break;
+          case '.png':
+               contentType = 'image/png';
+               break;
+          case '.jpg':
+               contentType = 'image/jpg';
+               break;
      }
-     if(req.url === '/api/users') {
-         const users = [
-              {name:'ASDFR', age: 23},
-              {name:'qqwee', age: 43}
-
-         ]
-         res.writeHead(200, {'Content-Type': 'application/json'})
-         res.end(JSON.stringify(users))
-     }
+     // Read file
+     fs.readFile(filePath,(err,content) => {
+          if(err) {
+               if(err.code === 'ENONT'){
+                    // Page not found
+                    fs.readFile(path.join(__dirname,'public','404.html'),(err,content)=>{
+                        res.writeHead(200, {'Content-Type': 'text/html'})
+                        res.end(content,'utf8')
+                    })
+               }else {
+                    // Some server error
+                    res.writeHead(500)
+                    req.end(`Server Error: ${err.code}`)
+               }
+          }else {
+               // Success
+               res.writeHead(200, {'Content-Type': contentType})
+               res.end(content,'utf8') 
+          }
+     })
 })
 
 const PORT = process.env.PORT || 5000
